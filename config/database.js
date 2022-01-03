8d0b70d786d788d2c146bbb253add420bfe70651
Luboš Svoboda <lubos.svoboda@gmail.com>
2022-01-03 18:39:01 +0100

    Heroku error fix

--- config/database.js
+++ config/database.js
@@ -25,14 +25,4 @@ module.exports = ({ env }) => {
       },
     }
   }
-
-  return {
-    connection: {
-      client: 'sqlite',
-      connection: {
-        filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
-      },
-      useNullAsDefault: true,
-    },
-  }
 };
