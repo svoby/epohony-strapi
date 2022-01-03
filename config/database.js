159a0746e70c7acc11411c2c616df4f80f8d4f1e
Luboš Svoboda <lubos.svoboda@gmail.com>
2022-01-03 21:13:43 +0100

    Postgress local config

--- config/database.js
+++ config/database.js
@@ -5,36 +5,65 @@ module.exports = ({ env }) => {
 
   if (env('NODE_ENV') === 'production') {
     const config = parse(process.env.DATABASE_URL);
-    console.log(config)
     return {
-      defaultConnection: 'default',
-      client: 'postgres',
-      connections: {
-        default: {
-          connector: 'bookshelf',
-          settings: {
-            client: 'postgres',
-            host: config.host,
-            port: config.port,
-            database: config.database,
-            username: config.user,
-            password: config.password,
-          },
-          options: {
-            ssl: false,
-          },
+      connection: {
+        client: 'postgres',
+        connection: {
+          host: config.host,
+          port: config.port,
+          database: config.database,
+          username: config.user,
+          password: config.password,
+          ssl: false,
         },
-      },
+      }
     }
   }
 
-  return {
-    connection: {
-      client: 'sqlite',
+  //   return {
+  //     defaultConnection: 'default',
+  //     connections: {
+  //       default: {
+  //         connector: 'bookshelf',
+  //         settings: {
+  //           client: 'postgres',
+  //           host: config.host,
+  //           port: config.port,
+  //           database: config.database,
+  //           username: config.user,
+  //           password: config.password,
+  //         },
+  //         options: {
+  //           ssl: false,
+  //         },
+  //       },
+  //     },
+  //   }
+  // }
+
+  if (env('NODE_ENV') === 'development') {
+    return {
       connection: {
-        filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
-      },
-      useNullAsDefault: true,
-    },
+        client: 'postgres',
+        connection: {
+          host: env('DATABASE_HOST', '127.0.0.1'),
+          port: env.int('DATABASE_PORT', 5432),
+          database: env('DATABASE_NAME', 'panzer'),
+          user: env('DATABASE_USERNAME', 'panzer'),
+          password: env('DATABASE_PASSWORD', 'panzer'),
+          ssl: env.bool('DATABASE_SSL', false),
+        },
+      }
+    }
   }
+
+  // return {
+  //   connection: {
+  //     client: 'sqlite',
+  //     connection: {
+  //       filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
+  //     },
+  //     useNullAsDefault: true,
+  //   },
+  // }
 };
