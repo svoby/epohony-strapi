6a44bd4b13220cd477af29c74218da6c4b475a9a
Luboš Svoboda <lubos.svoboda@gmail.com>
2022-01-03 17:59:05 +0100

    PostgresDB setup

--- config/database.js
+++ config/database.js
@@ -1,11 +1,48 @@
 const path = require('path');
+const parse = require('pg-connection-string').parse;
 
-module.exports = ({ env }) => ({
-  connection: {
-    client: 'sqlite',
+module.exports = ({ env }) => {
+
+  if (env('NODE_ENV') === 'production') {
+    const config = parse(process.env.DATABASE_URL)
+    return {
+      defaultConnection: 'default',
+      connections: {
+        default: {
+          connector: 'bookshelf',
+          settings: {
+            client: 'postgres',
+            host: config.host,
+            port: config.port,
+            database: config.database,
+            username: config.user,
+            password: config.password
+          },
+          options: {
+            ssl: false
+          }
+        }
+      }
+    }
+  }
+
+  return {
     connection: {
-      filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
+      client: 'sqlite',
+      connection: {
+        filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
+      },
+      useNullAsDefault: true,
     },
-    useNullAsDefault: true,
-  },
-});
+  }
+};
+
+// module.exports = ({ env }) => ({
+//   connection: {
+//     client: 'sqlite',
+//     connection: {
+//       filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
+//     },
+//     useNullAsDefault: true,
+//   },
+// });
